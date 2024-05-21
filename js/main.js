0b7160bfa2639348d269612395819228b64ad58a
Pistolgene <vbxer2019@gmail.com>
2024-05-21 11:17:54 +0200

    user add name interface changes

--- js/main.js
+++ js/main.js
@@ -6,3 +6,20 @@ document.addEventListener('DOMContentLoaded', function () {
   var gallery = document.querySelectorAll('.materialboxed');
   var instances_gallery = M.Materialbox.init(gallery);
 });
+
+let users = document.querySelector('.users__flex');
+function redrawUsers(obj) {}
+document.addEventListener('DOMContentLoaded', function (e) {
+  let user = prompt('Enter your Name');
+  if (user) {
+    localStorage.setItem(user, user);
+  }
+  for (let i = 0; i < localStorage.length; i++) {
+    users.insertAdjacentHTML(
+      'beforeend',
+      `
+      <h4 class='user__name'>${localStorage.key(i)}</h4>
+    `
+    );
+  }
+});
